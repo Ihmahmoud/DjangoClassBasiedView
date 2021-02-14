b5aefe9bf5fe525f99a79243bab1051e45f7399f
Ihmahmoud <77974878+Ihmahmoud@users.noreply.github.com>
2021-02-14 13:45:37 +0300

    class Based Views

--- post/urls.py
+++ post/urls.py
@@ -5,6 +5,12 @@ from .import views
 app_name = 'post'
 
 urlpatterns = [
-    path('',views.post_list),
-    path('<int:id>',views.post_details),
+    # path('',views.post_list, name='post_list'),
+    # path('<int:id>',views.post_details, name='post_detail'),
+    
+    
+    #### for class
+    
+    path('',views.PostList.as_view(), name='post_list'),
+    path('<int:pk>',views.PostDetail.as_view(), name='post_detail'),
 ]
--- post/views.py
+++ post/views.py
@@ -1,10 +1,47 @@
 from django.shortcuts import render
 
+
+
+
 # Create your views here.
 
 
-def post_list(request):
+from .models import Post
+
+from django.views.generic import ListView, DetailView, CreateView, UpdateView, DeleteView
+
+# def post_list(request):
+#     post_list = Post.objects.all()
+#     return render(request , 'post/list.html',{'post_list': post_list})
+
+# def post_details(request,id):
+#     pass
+
+
+class PostList(ListView):
+    model = Post
+
+
+
+
+class PostDetail(DetailView):
+    model = Post
+
+
+
+class PostCreate():
     pass
 
-def post_details(request,id):
+
+
+class PostDelete():
     pass
+
+
+
+
+class PostUpdate():
+    pass 
+
+
+
